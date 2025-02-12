6a8076c3ee30447243a8c5074e832a5acf4a16fe
Lucas Zaranza <lucaszaranza@gmail.com>
2025-02-12 10:33:47 -0300

    fix path alias

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import Image from "next/image";
-import Header from "./components/sections/header/header";
-import AboutMe from "./components/sections/about-me/about-me";
-import WorkAreas from "./components/sections/work-areas/work-areas";
-import CoreValues from "./components/sections/core-values/core-values";
-import SAC from "./components/sections/sac/sac";
-import Instagram from "./components/sections/instagram/instagram";
-import ContactForm from "./components/sections/contact-form/contact-form";
-import Footer from "./components/sections/footer/footer";
+import Header from "@/components/sections/header"
+import AboutMe from "@/components/sections/about-me";
+import WorkAreas from "@/components/sections/work-areas";
+import CoreValues from "@/components/sections/core-values";
+import SAC from "@/components/sections/sac";
+import Instagram from "@/components/sections/instagram";
+import ContactForm from "@/components/sections/contact-form";
+import Footer from "@/components/sections/footer";
 
 export default function Home() {
   return (
