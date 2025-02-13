2cb8e243dc7be9d5ee9e5e421bbcd3b7f89d24de
Lucas Zaranza <lucaszaranza@gmail.com>
2025-02-12 23:08:00 -0300

    added 3-grid footer

--- src/app/components/sections/header/index.tsx
+++ src/app/components/sections/header/index.tsx
@@ -1,7 +1,9 @@
 export default function Header() {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="grid grid-cols-3 h-6">
+      <div className="bg-lighter"></div>
+      <div className="bg-primary"></div>
+      <div className="bg-darker"></div>
     </div>
   )
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,9 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#C850D3",
+        darker: "#84099F",
+        lighter: "#EAB1F6"
       },
     },
   },
