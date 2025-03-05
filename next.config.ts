aec9a8e4f60629a08f35d2c18ba7adfcd96a49c5
Lucas Zaranza <lucaszaranza@gmail.com>
2025-03-05 20:11:35 -0300

    removing GH pages configs at next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "export", // Exporta como HTML estático
-  basePath: "/elisa-adv", // Define o caminho base do GitHub Pages
-  images: {
-    unoptimized: true, // Evita otimizações de imagem (GitHub Pages não suporta a otimização automática do Next.js)
-  },
 };
 
 export default nextConfig;
